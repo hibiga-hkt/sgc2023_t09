b50b3f2a791c8e7c93f5f47c911e286b9fa46d5b
丹野竜之介 <221047@st.yoshida-g.ac.jp>
2023-10-07 16:03:22 +0900

    タイマー追加

---
+++ project/Assets/Script/TimerManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimerManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject m_TimeText;
+    [SerializeField]
+    public float fTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float TimeCount = fTime - Time.time;
+        if(TimeCount <= 0)
+        {
+            TimeCount = 0;
+        }
+
+    
+        m_TimeText.GetComponent<Text>().text = "TIME:" + TimeCount.ToString("00.0");
+    }
+}
