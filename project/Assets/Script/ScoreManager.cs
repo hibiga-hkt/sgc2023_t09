32de79d43d74ba6a79d70c5e63ad7ad0d6301247
丹野竜之介 <221047@st.yoshida-g.ac.jp>
2023-10-07 15:20:00 +0900

    スコア表示追加

---
+++ project/Assets/Script/ScoreManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject m_scoreText;
+    private int nSunFish;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nSunFish = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddScore(int addScore)
+    {
+        nSunFish += addScore;
+        m_scoreText.GetComponent<Text>().text = "Score:" + this.nSunFish.ToString("D9");
+    }
+}
