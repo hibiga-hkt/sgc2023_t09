c5e309d470b87634b52a62e6ad6de0750d185b31
Ibuki Okusada <221038@st.yoshida-g.ac.jp>
2023-10-08 14:31:17 +0900

    ゲームマネージャー更新

--- project/Assets/Script/Item_TimeBonus.cs
+++ project/Assets/Script/Item_TimeBonus.cs
@@ -16,7 +16,7 @@ public class Item_TimeBonus : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_Life = 1000;
+      
     }
 
     // Update is called once per frame
